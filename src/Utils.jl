af8cea536f82617efc9573242e0905f011ff50be
Mechtanium <a.sapiens.view@gmail.com>
2023-01-28 23:44:47 +0100

    Fix manifest and failing tests

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,5 +1,5 @@
 sayhi = name -> "Dear $name, I'm Julia and I'd like to say hi!"
 
-greetme = name -> "Hello $name, nice to meet you"
+greetme = name -> "Hello $name, nice to meet you. I'm Julia"
 
 greetmeinspanish = nombre -> "Hola senor $nombre. Como estas. Me llamo Julia y tengo una hermana, se llama Julietta"
